fbe91b43e817ad95170f4dca7551a8edf9924d46
Abhishek Jee <abhishek.jee.2411@gmail.com>
2018-02-12 18:09:28 +0530

    Commit

---
+++ partitions_and_views.sql
@@ -0,0 +1,6 @@
+https://docs.oracle.com/database/121/CNCPT/schemaob.htm#CNCPT88858
+
+"=========================================="
+"PARTITIONS, VIEWS and OTHER SCHEMA OBJECTS"
+"=========================================="
+
---
+++ tables_and_table_clusters.sql
@@ -0,0 +1,9 @@
+https://docs.oracle.com/database/121/CNCPT/tablecls.htm#CNCPT88801
+
+"========================="
+"TABLES AND TABLE CLUSTERS"
+"========================="
+
+https://docs.oracle.com/database/121/CNCPT/toc.htm
+Oracle Database 12c
+
